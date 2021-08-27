3054697a8351eef52b4169f89ea4ed861b96234e
Quazariun <quazariun@gmail.com>
2021-08-27 12:04:21 -0300

    Add 2718 problem

---
+++ URI/2718/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+	unsigned long long lamps;
+	int range_count;
+	int max_range;
+
+	int n;
+	scanf("%d", &n);
+
+	for(int x = 0; x < n; ++x){
+		range_count = 0;
+		max_range = 0;
+		scanf("%llu", &lamps);
+
+		while(1){
+			if(lamps % 2 == 1){
+				range_count++;
+			}else{
+				if( range_count > max_range)
+					max_range = range_count;
+				range_count = 0;
+			}
+			if( lamps / 2 <= 0){
+				if( range_count > max_range)
+					max_range = range_count;
+				break;
+			}
+			lamps /= 2; 
+		}
+		printf("%d\n", max_range);
+	}
+
+	return 0;
+}
