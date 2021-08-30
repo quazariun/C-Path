d7a05eaea15be07f5afe9642a8a0eb4c6c296a7e
Quazariun <quazariun@gmail.com>
2021-08-30 08:35:27 -0300

    Add 2748 problem

---
+++ URI/2748/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void hor_line(){
+	for(int x = 0; x < 39; ++x)
+		printf("-");
+	printf("\n");
+}
+
+void ver_line(char *str, int size){
+	int i = 0;
+	
+	printf("|");
+	for(int x = 0; x < 37; ++x){
+		if(x >= 8 && size > 0){
+			printf("%c", str[i++]);
+			size--;
+		}else{
+			printf(" ");
+		}
+	}
+	printf("|\n");
+}
+
+int main(){
+
+	hor_line();
+	ver_line("Roberto",7);
+	ver_line("", 0);
+	ver_line("5786", 4);
+	ver_line("", 0);
+	ver_line("UNIFEI", 6);
+	hor_line();
+	
+	return 0;
+}
