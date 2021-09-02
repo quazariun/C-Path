34673d7327d40b218c7bb8df474b0c3cfc26bf5e
Quazariun <quazariun@gmail.com>
2021-09-02 13:19:08 -0300

    Add 2709 problem

---
+++ URI/2709/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int sum(int coins[40], int step, int size){
+  int result = 0;
+
+  for(int x = size-1; x >= 0; x -= step){
+    result += coins[x];
+  }
+  return result;
+}
+
+int isprime(int num){
+  int root = (int)sqrt(num);
+
+  if(num != 2 && num % 2 == 0 || num == 1) return 0;
+
+  for(int x = 3; x <= root; x += 2){
+    if(num % x == 0){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int main(){
+  int n;
+  int coins[40];
+  int step;
+
+  while(scanf("%d", &n) == 1){
+    for(int x = 0; x < n; ++x)
+      scanf("%d", &coins[x]);
+
+    scanf("%d", &step);
+    if(isprime(sum(coins,step,n))){
+      printf("You’re a coastal aircraft, Robbie, a large silver aircraft.\n");
+    }else{
+      printf("Bad boy! I’ll hit you.\n");
+    }
+  }
+
+  return 0;
+}
