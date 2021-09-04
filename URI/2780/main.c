c245eb0863572f762989702aabab3e62145913d3
Quazariun <quazariun@gmail.com>
2021-09-04 10:13:14 -0300

    Add 2780 problem

---
+++ URI/2780/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int dist;
+  scanf("%d", &dist);
+
+  if(dist <= 800){
+    printf("1\n");
+  }else if(dist > 800 && dist <= 1400){
+    printf("2\n");
+  }else{
+    printf("3\n");
+  }
+
+  return 0;
+}
